7c5c6de4ed80836c8c9441e421d8a4836eda8e3d
KallDrexx <me@mshapiro.net>
2026-09-24 22:04:20 -0400

    Multi-run benchmark wip

--- src/Dotnet6502.Benchmark/Program.cs
+++ src/Dotnet6502.Benchmark/Program.cs
@@ -3,102 +3,144 @@ using Dotnet6502.Benchmark;
 using Dotnet6502.Common.Compilation;
 using Dotnet6502.Nes;
 
-var options = CommandLineHandler.Parse(args);
-if (options == null)
+namespace Dotnet6502.Benchmark;
+
+public static class Program
 {
-    return 1;
-}
+    private record RunInterval(TimeSpan Timing, int FrameCount);
 
-ISystem system;
-if (options.NesConfig != null)
-{
-    system = new NesSystem(options.NesConfig);
-}
-else
-{
-    Console.Error.WriteLine("No known system specified to benchmark");
-    return 1;
-}
-
-var jitCustomizer = new NesJitCustomizer();
-var interpreter = new Ir6502Interpreter();
-jitCustomizer.AddInstructions(interpreter);
-
-var jitCompiler = new JitCompiler(system.Hal, jitCustomizer, system.MemoryBus, interpreter);
-
-var framesPerInterval = options.FramesPerInterval ?? 60;
-long frameCount = 0;
-var stopwatch = new Stopwatch();
-var timings = new Queue<(TimeSpan Timing, int FrameCount)>((int)Math.Ceiling((decimal)options.FrameCount / framesPerInterval));
-
-system.OnFrameFinished = () =>
-{
-    if (!stopwatch.IsRunning)
+    public static int Main(string[] args)
     {
-        // First frame is missed for accuracy
-        stopwatch.Start();
-    }
-    else
-    {
-        frameCount++;
-
-        var isNewInterval = false;
-        if (frameCount % framesPerInterval == 0)
+        var options = CommandLineHandler.Parse(args);
+        if (options == null)
         {
-            stopwatch.Stop();
-            timings.Enqueue((stopwatch.Elapsed, framesPerInterval));
-            stopwatch.Restart();
-            isNewInterval = true;
+            return 1;
         }
 
-        if (frameCount >= options.FrameCount)
+        var framesPerInterval = options.FramesPerInterval ?? 60;
+        const int runCount = 5;
+        var runs = new Queue<RunInterval>[runCount];
+
+        Console.WriteLine($"Starting {runCount} benchmark runs");
+        for (var x = 0; x < runCount; x++)
         {
-            stopwatch.Stop();
-            if (!isNewInterval)
+            var runInfo = RunBenchmark(options);
+            if (runInfo == null)
             {
-                timings.Enqueue((stopwatch.Elapsed, (int)frameCount % framesPerInterval));
+                return 1;
             }
-            
-            system.CodeCancellationTokenSource.Cancel();
+
+            runs[x] = runInfo;
         }
+
+        Console.WriteLine($"Benchmark finished");
+        Console.WriteLine($"Frames Executed: {options.FrameCount}");
+        Console.WriteLine($"Frames Per Interval: {framesPerInterval}");
+        Console.WriteLine();
+
+        var intervalCount = runs[0].Count;
+        if (runs.Any(x => x.Count != intervalCount))
+        {
+            Console.Error.WriteLine($"All runs do not have the same number of intervals");
+            return 1;
+        }
+
+        for (var x = 0; x < intervalCount; x++)
+        {
+            
+        }
+
+
+        var intervalNum = 0;
+        while (timings.TryDequeue(out var data))
+        {
+            intervalNum++;
+            var averageTime = data.Timing.TotalMilliseconds / data.FrameCount;
+
+            Console.Write($"{intervalNum:000}: ");
+            Console.Write($"average: {averageTime:00.00}ms ");
+            Console.Write($"total: {data.Timing.TotalMilliseconds:000.00}ms ");
+
+            if (data.FrameCount != framesPerInterval)
+            {
+                Console.Write($"({data.FrameCount} frames)");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        return 0;
     }
-};
 
-Console.WriteLine($"Starting benchmark");
-var resetVector = system.GetResetVector();
-
-try
-{
-    jitCompiler.RunMethod(resetVector);
-}
-catch (TaskCanceledException)
-{
-    // Expected
-}
-
-Console.WriteLine($"Benchmark finished");
-Console.WriteLine($"Frames Executed: {frameCount}");
-Console.WriteLine($"Frames Per Interval: {framesPerInterval}");
-Console.WriteLine();
-
-var intervalNum = 0;
-while (timings.TryDequeue(out var data))
-{
-    intervalNum++;
-    var averageTime = data.Timing.TotalMilliseconds / data.FrameCount;
-
-    Console.Write($"{intervalNum:000}: ");
-    Console.Write($"average: {averageTime:00.00}ms ");
-    Console.Write($"total: {data.Timing.TotalMilliseconds:000.00}ms ");
-
-
-    if (data.FrameCount != framesPerInterval)
+    private static Queue<RunInterval>? RunBenchmark(CommandLineHandler.Options options)
     {
-        Console.Write($"({data.FrameCount} frames)");
+        ISystem system;
+        if (options.NesConfig != null)
+        {
+            system = new NesSystem(options.NesConfig);
+        }
+        else
+        {
+            Console.Error.WriteLine("No known system specified to benchmark");
+            return null;
+        }
+
+        var jitCustomizer = new NesJitCustomizer();
+        var interpreter = new Ir6502Interpreter();
+        jitCustomizer.AddInstructions(interpreter);
+
+        var jitCompiler = new JitCompiler(system.Hal, jitCustomizer, system.MemoryBus, interpreter);
+
+        var framesPerInterval = options.FramesPerInterval ?? 60;
+        long frameCount = 0;
+        var stopwatch = new Stopwatch();
+        var timings = new Queue<RunInterval>((int)Math.Ceiling((decimal)options.FrameCount / framesPerInterval));
+
+        system.OnFrameFinished = () =>
+        {
+            if (!stopwatch.IsRunning)
+            {
+                // First frame is missed for accuracy
+                stopwatch.Start();
+            }
+            else
+            {
+                frameCount++;
+
+                var isNewInterval = false;
+                if (frameCount % framesPerInterval == 0)
+                {
+                    stopwatch.Stop();
+                    timings.Enqueue(new RunInterval(stopwatch.Elapsed, framesPerInterval));
+                    stopwatch.Restart();
+                    isNewInterval = true;
+                }
+
+                if (frameCount >= options.FrameCount)
+                {
+                    stopwatch.Stop();
+                    if (!isNewInterval)
+                    {
+                        timings.Enqueue(new RunInterval(stopwatch.Elapsed, (int)frameCount % framesPerInterval));
+                    }
+
+                    system.CodeCancellationTokenSource.Cancel();
+                }
+            }
+        };
+
+        var resetVector = system.GetResetVector();
+
+        try
+        {
+            jitCompiler.RunMethod(resetVector);
+        }
+        catch (TaskCanceledException)
+        {
+            // Expected
+        }
+
+        return timings;
     }
-
-    Console.WriteLine();
 }
-
-return 0;
-
